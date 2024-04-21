9610e3dcbf05bab77ae45cb45e3891ad84ab4634
Laborratte <laborratte@cavity.at>
2024-04-21 19:54:02 +0200

    add platform detection

--- server/Program.cs
+++ server/Program.cs
@@ -3,19 +3,24 @@ using Python.Runtime;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR()
-    .AddMessagePackProtocol();
+	.AddMessagePackProtocol();
 builder.Services.AddCors(options =>
-    options.AddDefaultPolicy(builder =>
-        builder
-            .WithOrigins("http://localhost:3000")
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            .AllowCredentials()
-    )
+	options.AddDefaultPolicy(builder =>
+		builder
+			.WithOrigins("http://localhost:3000")
+			.AllowAnyMethod()
+			.AllowAnyHeader()
+			.AllowCredentials()
+	)
 );
 builder.Services.AddSingleton<DeviceManager>();
 
-Runtime.PythonDLL = "/Library/Frameworks/Python.framework/Versions/3.12/lib/libpython3.12.dylib";
+Runtime.PythonDLL = Environment.OSVersion.Platform switch
+{
+	PlatformID.Win32NT => @"C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.12_3.12.1008.0_x64__qbz5n2kfra8p0\python312.dll",
+	PlatformID.Unix => "/Library/Frameworks/Python.framework/Versions/3.12/lib/libpython3.12.dylib",
+	_ => null,
+};
 PythonEngine.Initialize();
 PythonEngine.BeginAllowThreads();
 
