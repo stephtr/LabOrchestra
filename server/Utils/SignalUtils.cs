b9e5af60c7e53ef8994143a027cc4fafef200612
Stephan Troyer <stephantroyer@live.at>
2024-05-06 14:58:44 +0200

    switch to min/max decimation

--- server/Utils/SignalUtils.cs
+++ server/Utils/SignalUtils.cs
@@ -3,34 +3,44 @@ using NumSharp.Utilities;
 
 public static class SignalUtils
 {
-    public static (T[] signal, float xMin, float xMax) DecimateSignal<T>(T[] signal, float xMin, float xMax, float xStart, float xEnd, int maxSamples) where T : INumber<T>
-    {
-        if (xMax == xMin || signal.Length < 2) return (Array.Empty<T>(), xMin, xMin);
-        if (xStart < xMin) xStart = xMin;
-        if (xEnd > xMax) xEnd = xMax;
-        var dx = (xMax - xMin) / (signal.Length - 1);
-        var iStart = (int)Math.Floor((xStart - xMin) / dx);
-        var count = (int)Math.Ceiling((xEnd - xStart) / dx);
+	public static (T[] signal, float xMin, float xMax) DecimateSignal<T>(T[] signal, float xMin, float xMax, float xStart, float xEnd, int maxSamples) where T : INumber<T>
+	{
+		if (xMax == xMin || signal.Length < 2) return (Array.Empty<T>(), xMin, xMin);
+		if (xStart < xMin) xStart = xMin;
+		if (xEnd > xMax) xEnd = xMax;
+		var dx = (xMax - xMin) / (signal.Length - 1);
+		var iStart = (int)Math.Floor((xStart - xMin) / dx);
+		var count = (int)Math.Ceiling((xEnd - xStart) / dx);
 
-        var decimation = (int)(count / maxSamples);
-        if (decimation <= 1)
-        {
-            return (signal.Slice(iStart, iStart + count), xMin + iStart * dx, xMin + (iStart + count - 1) * dx);
-        }
-        iStart = Math.Max(0, iStart - decimation / 2);
-        count = Math.Min(signal.Length - iStart, count + decimation);
-        var decimatedSignal = new T[count / decimation];
-        T buffer = T.Zero;
-        var decimationAsT = T.CreateChecked(decimation);
-        for (var i = 0; i < count; i++)
-        {
-            buffer += signal[i + iStart];
-            if ((i % decimation) == decimation - 1)
-            {
-                decimatedSignal[i / decimation] = buffer / decimationAsT;
-                buffer = T.Zero;
-            }
-        }
-        return (decimatedSignal, xMin + (iStart + (decimation - 1) / 2f) * dx, xMin + (iStart + count - 1 - (decimation - 1) / 2f) * dx);
-    }
+		var decimation = (int)(2 * count / maxSamples);
+		if (decimation <= 2)
+		{
+			return (signal.Slice(iStart, iStart + count), xMin + iStart * dx, xMin + (iStart + count - 1) * dx);
+		}
+		iStart = Math.Max(0, iStart - decimation / 2);
+		count = Math.Min(signal.Length - iStart, count + decimation);
+		var decimatedSignal = new T[count / decimation];
+		T minVal = signal[iStart];
+		T maxVal = signal[iStart];
+		var decimationAsT = T.CreateChecked(decimation);
+		for (var i = 1; i < count; i++)
+		{
+			var val = signal[i + iStart];
+			if (val < minVal) minVal = val;
+			if (val > maxVal) maxVal = val;
+			if ((i % decimation) == decimation - 1)
+			{
+				decimatedSignal[2 * i / decimation] = minVal;
+				decimatedSignal[2 * i / decimation + 1] = minVal;
+				if (i + iStart + 1 < signal.Length)
+				{
+					val = signal[i + iStart + 1];
+					minVal = val;
+					maxVal = val;
+					i++;
+				}
+			}
+		}
+		return (decimatedSignal, xMin + (iStart + (decimation - 1) / 2f) * dx, xMin + (iStart + count - 1 - (decimation - 1) / 2f) * dx);
+	}
 }
